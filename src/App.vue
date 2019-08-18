75d482fb65ee52981672af10f1af8872f183b612
LiuJian0806 <13692591081@163.com>
2019-08-18 20:04:18 +0800

    2019.8.18

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,206 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">主页</router-link> |
-      <router-link to="/about">关于我们</router-link>
-      <div class="app">
-        <h1>第一个vue项目</h1>
-        <p>这是一个vue项目，你不要管，反正这个项目很大</p>
+  <div class="home-container">
+    <!-- 顶部 -->
+    <div class="header" v-show="false">
+      <div class="left">
+        <div class="profile-photo"></div>
+        <div class="address-weather">
+          <div class="address">
+            广州
+            <span class="icon iconfont">&#xe63c;</span>
+          </div>
+          <div class="weather">晴 30&#176;</div>
+        </div>
+      </div>
+      <div class="middle">
+        <mt-search v-model="value"></mt-search>
+      </div>
+      <div class="right">
+        <span class="icon iconfont add">&#xe613;</span>
       </div>
     </div>
-    <!-- <router-view/> -->
+    <!-- 选择地址的头部 -->
+    <div class="address-header" v-show="true">
+        <div class="search">
+         <span class="icon iconfont back">&#xe677;</span>
+         <div class="search-input"><mt-search v-model="value"></mt-search></div>
+        </div>
+        <div class="navigation-button">
+          <!-- <div class="inland">国内</div>
+          <div class="outland">国际/港澳台</div> -->
+          <mt-navbar v-model="selected">
+            <mt-tab-item id="1">国内</mt-tab-item>
+            <mt-tab-item id="2">国际/港澳台</mt-tab-item>
+          </mt-navbar>
+        </div>
+    </div>
+
+    <router-view class="content"></router-view>
+    <!-- 底部导航栏 -->
+    <mt-tabbar v-model="selected">
+      <mt-tab-item id="tab1">
+        <span slot="icon" class="icon iconfont navigation">&#xe667;</span>
+        首页
+      </mt-tab-item>
+      <mt-tab-item id="tab2">
+        <span slot="icon" class="icon iconfont navigation">&#xe61f;</span>
+        搜索
+      </mt-tab-item>
+      <mt-tab-item id="tab3">
+        <span slot="icon" class="icon iconfont navigation">&#xe69d;</span>
+        订单
+      </mt-tab-item>
+      <mt-tab-item id="tab4">
+        <span slot="icon" class="icon iconfont navigation">&#xe605;</span>
+        我的
+      </mt-tab-item>
+    </mt-tabbar>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  data () {
+    return {
+      value: '',
+      selected: ''
+    }
+  },
+  methods: {}
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+</script>
+<style lang="scss" >
+.home-container {
+  .header {
+    position:fixed;
+    width: 100%;
+    height: 50px;
+    display: flex;
+    justify-content: space-between;
+    background-color: #d9d9d9;
+    .left {
+      width: 30%;
+      height: 100%;
+      display: flex;
+      justify-content: space-between;
+      .profile-photo {
+        min-width: 30px;
+        min-height: 30px;
+        max-width: 30px;
+        max-height: 30px;
+        margin: 10px 10px;
+        border-radius: 50%;
+        background-image: url("./assets/background.jpg");
+        background-size: 100% 100%;
+        overflow: hidden;
+        img {
+          width: 100%;
+          height: 100%;
+        }
+      }
+      .address-weather {
+        width: 70%;
+        height: 80%;
+        margin-top: 5px;
+        .address {
+          font-size: 14px;
+          line-height: 22px;
+          font-weight: bold;
+        }
+        .weather {
+          font-size: 12px;
+          line-height: 15px;
+        }
+      }
+    }
+    .middle {
+      width: 55%;
+      height: 100%;
+      .mint-search {
+        height: 50px;
+        padding: 3px 5px;
+      }
+    }
+    .right {
+      width: 10%;
+      height: 100%;
+      .add {
+        font-size: 18px;
+        line-height: 50px;
+        padding-left: 10px;
+      }
+    }
+  }
+  .mint-tabbar{
+    position: fixed;
+    bottom: 0;
+  }
+  .is-selected{
+    color: skyblue !important;
+  }
+  .navigation{
+    font-size: 20px !important;
+  }
+  // router-view
+  .content{
+    padding-top:50px;
+    padding-bottom: 55px; 
+  }
+
+  //selected-address
+  .address-header{
+    position: fixed;
+    height: 80px;
+    width: 100%;
+    background-color: #d9d9d9;
+    .search{
+      height: 45px;
+      display: flex;
+      justify-content: space-between;
+      .back{
+        font-size: 20px;
+        margin-left: 10px;
+        line-height: 45px;
+      }
+      .search-input{
+        width: 90%;
+        height: 100%;
+      }
+    }
+    .navigation-button{
+      height: 35px;
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+      
+      text-align: center;
+
+      .mint-navbar{
+        width: 100%;
+        .mint-tab-item{
+          padding:0;
+          .mint-tab-item-label{
+            line-height: 35px !important;
+            font-size: 18px !important;
+            padding: 0;
+          }
+        }
+        
+      }
+      .inland{
+        width: 50%;
+      }
+      .outland{
+        width: 50%;
+      }
+      .inland:hover{
+        color: rgb(34, 146, 238);
+        border-bottom: 3px solid rgb(34, 146, 238);
+      }
+      .outland:hover{
+        color: rgb(34, 146, 238);
+        border-bottom: 3px solid rgb(34, 146, 238);
+      }
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -2,6 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { Header, Button, Search, Tabbar, TabItem, Navbar } from 'mint-ui'
+import 'mint-ui/lib/style.css'
+// 使用bootstrap图标
+import 'bootstrap/dist/css/bootstrap.css'
+import 'open-iconic/font/css/open-iconic-bootstrap.css'
+// 使用阿里图标
+import '@/assets/iconfont/iconfont.css'
+
+Vue.component(Header.name, Header)
+Vue.component(Button.name, Button)
+Vue.component(Search.name, Search)
+Vue.component(Tabbar.name, Tabbar)
+Vue.component(TabItem.name, TabItem)
+Vue.component(Navbar.name, Navbar)
 
 Vue.config.productionTip = false
 
